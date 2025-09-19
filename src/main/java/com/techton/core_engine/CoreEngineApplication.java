6278ef2a7bdd918b6f87c59c96f190cd0f0aee68
sooraj pn <soorajpn23@gmail.com>
2025-09-19 18:42:12 +0530

    second commit

---
+++ src/main/java/com/techton/core_engine/CoreEngineApplication.java
@@ -0,0 +1,13 @@
+package com.techton.core_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techton/core_engine/CoreEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techton.core_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
